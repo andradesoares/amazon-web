82b9280024353eb07636c95c55c38ad39c0068a9
Daniel Soares <danielandradesoares@hotmail.com>
2022-09-28 14:27:45 -0300

    reset card reducer

--- src/features/product/productSlice.ts
+++ src/features/product/productSlice.ts
@@ -71,6 +71,9 @@ export const productSlice = createSlice({
       const modifiedCart = modifyQtyByOne(state.cart, action.payload, 'DECREMENT');
       state.cart = modifiedCart;
     },
+    resetCart: (state) => {
+      state.cart = [];
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -90,6 +93,6 @@ export const productSlice = createSlice({
   },
 });
 
-export const { incrementProduct, decrementProduct } = productSlice.actions;
+export const { incrementProduct, decrementProduct, resetCart } = productSlice.actions;
 
 export default productSlice.reducer;
